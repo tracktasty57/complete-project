cbee7eb7068b67fe9fe80a0c2a08c05cf92c057f
NOOR FATIMA <noor.fatima.212212@gmail.com>
2025-12-27 20:59:10 +0500

    Fix ESLint Flat Config with FlatCompat and update next.config.js for ESM/CJS compatibility

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    // REMOVED REWRITES FROM HERE - vercel.json will handle them for the entire repo
-};
-
-module.exports = nextConfig;
